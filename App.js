ade9b90d87f7937701754131ed001163b08f69af
Luis Gustavo <gustascrows@gmail.com>
2021-02-11 17:05:02 -0300

    Assets: App.js do projeto

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import MainStack from './src/navigator/MainStack';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  );
+}
